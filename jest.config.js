dfa3dfa1bf918faff1adf20079d95819e573344e
Lourene Schueler <lourenecamargo@hotmail.com>
2022-10-27 17:43:25 -0300

    refactor: remove collectCoverageFrom config and adapters folder

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/config/**',
+    '!<rootDir>/src/main/adapters',
+    '!<rootDir>/src/main/server.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'jest-environment-node',
   preset: '@shelf/jest-mongodb',
   transform: {
