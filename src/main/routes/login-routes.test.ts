fe207009979fad1b80b67fd17e60aa5ccc39013f
Lourene Schueler <lourenecamargo@hotmail.com>
2022-11-12 09:50:52 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -16,15 +16,17 @@ describe('Signup Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Lou',
-        email: 'lourene.camargo@gmail.com',
-        password: '159753',
-        passwordConfirmation: '159753'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Lou',
+          email: 'lourene.camargo@gmail.com',
+          password: '159753',
+          passwordConfirmation: '159753'
+        })
+        .expect(200)
+    })
   })
 })
