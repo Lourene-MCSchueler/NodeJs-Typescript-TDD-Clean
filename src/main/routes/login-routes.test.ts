cc13332176fffac16f2efd83735b6c9726dee1c4
Lourene Schueler <lourenecamargo@hotmail.com>
2022-11-12 10:18:09 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lourene.camargo@gmail.com',
+          password: '159753'
+        })
+        .expect(401)
+    })
   })
 })
