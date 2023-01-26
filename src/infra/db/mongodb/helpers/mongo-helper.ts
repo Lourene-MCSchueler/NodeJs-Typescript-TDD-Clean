5e14d613156c517f0c34423495faa19d03570c53
Lourene Schueler <lourenecamargo@hotmail.com>
2023-01-26 09:37:24 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -24,5 +24,9 @@ export const MongoHelper = {
   map(collection: any): any {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
+  },
+
+  mapCollection(collection: any[]): any[] {
+    return collection.map(c => MongoHelper.map(c))
   }
 }
