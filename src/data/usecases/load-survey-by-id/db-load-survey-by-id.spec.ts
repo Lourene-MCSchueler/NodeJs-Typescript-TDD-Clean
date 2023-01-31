c0660fd271529e65dca3e33a1a9b5ed4639451f6
Lourene Schueler <lourenecamargo@hotmail.com>
2023-01-30 22:25:39 -0300

    refactor: use protocol files to imports

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -1,6 +1,5 @@
-import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
-import { DbLoadSurveyById } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id'
-import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyByIdRepository, SurveyModel } from './db-load-survey-by-id-protocols'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
 import MockDate from 'mockdate'
 
 const makeFakeSurvey = (): SurveyModel => {
@@ -38,7 +37,7 @@ const makeSut = (): SutTypes => {
   }
 }
 
-describe('DbLoadSurveys', () => {
+describe('DbLoadSurveyById', () => {
   beforeAll(() => {
     MockDate.set(new Date())
   })
